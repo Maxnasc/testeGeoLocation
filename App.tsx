49f04bb64b5c36c333da43d82fc5110d07aa9f47
Max Deivid <56174024+Maxnasc@users.noreply.github.com>
2023-11-16 09:26:19 -0300

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+  LocationObject,
+  LocationAccuracy,
+  watchPositionAsync
+} from 'expo-location';
+import { styles } from './Styles';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [location, setLocation] = useState<LocationObject | null>(null);
+
+  async function requestLocationPermission() {
+    const { granted } = await requestForegroundPermissionsAsync();
+
+    if (granted) {
+      const currentPosition = await getCurrentPositionAsync();
+      setLocation(currentPosition);
+    }
+  }
+
+  useEffect(() => { requestLocationPermission(); }, []);
+  useEffect(() => {
+    watchPositionAsync({
+      accuracy: LocationAccuracy.Highest, timeInterval: 1000,
+      distanceInterval: 1
+    }, (response) => {
+      //console.log('Nova Localização', response);
+      setLocation(response);
+      
+    })
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+          />
+        </MapView>
+      }
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
