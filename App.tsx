5ad76383b3b801ba330d0f9565427ad2160999fa
Max Deivid <56174024+Maxnasc@users.noreply.github.com>
2023-11-21 20:33:58 -0300

    app funcionadno

--- App.tsx
+++ App.tsx
@@ -2,16 +2,18 @@ import { View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import {
   requestForegroundPermissionsAsync,
+  requestBackgroundPermissionsAsync,
   getCurrentPositionAsync,
   LocationObject,
   LocationAccuracy,
   watchPositionAsync
 } from 'expo-location';
 import { styles } from './Styles';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 export default function App() {
   const [location, setLocation] = useState<LocationObject | null>(null);
+  const mapRef = useRef<MapView>(null);
 
   async function requestLocationPermission() {
     const { granted } = await requestForegroundPermissionsAsync();
@@ -19,25 +21,41 @@ export default function App() {
     if (granted) {
       const currentPosition = await getCurrentPositionAsync();
       setLocation(currentPosition);
+    } else {
+      console.log('Permissão de localização em primeiro plano negada');
+    }
+
+    const { granted: backgroundGranted } = await requestBackgroundPermissionsAsync();
+
+    if (!backgroundGranted) {
+      console.log('Permissão de localização em segundo plano negada');
     }
   }
 
-  useEffect(() => { requestLocationPermission(); }, []);
   useEffect(() => {
-    watchPositionAsync({
-      accuracy: LocationAccuracy.Highest, timeInterval: 1000,
-      distanceInterval: 1
-    }, (response) => {
-      //console.log('Nova Localização', response);
-      setLocation(response);
-      
-    })
+    requestLocationPermission();
+  }, []);
+
+  useEffect(() => {
+    watchPositionAsync(
+      {
+        accuracy: LocationAccuracy.Highest,
+        timeInterval: 1000,
+        distanceInterval: 1
+      },
+      (response) => {
+        setLocation(response);
+        console.log('Localização atualizada: ' + response.coords.latitude + ' | ' + response.coords.longitude);
+        mapRef.current?.animateCamera({ center: response.coords });
+      }
+    );
   }, []);
 
   return (
     <View style={styles.container}>
-      {
+      {location && ( // Renderiza condicionalmente apenas quando location não é null
         <MapView
+          ref={mapRef}
           style={styles.map}
           initialRegion={{
             latitude: location.coords.latitude,
@@ -49,12 +67,11 @@ export default function App() {
           <Marker
             coordinate={{
               latitude: location.coords.latitude,
-              longitude: location.coords.longitude,
+              longitude: location.coords.longitude
             }}
           />
         </MapView>
-      }
+      )}
     </View>
   );
 }
-
